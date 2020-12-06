7d1bc03dbe05a4c00fc4f74d71c3d91a434dcb57
MaureenJoy <73148238+MaureenJoy@users.noreply.github.com>
2020-12-06 11:47:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,41 @@
-<?php include_once("index.html"); ?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+<meta name="viewport" content="width=device-witdh, initial scale=1">
+<title>Main</title>
+<link rel="stylesheet" type="text/css" href="mjstyle.css">
+
+</head>
+<body class="bg">
+    <header >
+    <img class="logo" src="logo.png" alt="logo" >
+    <nav class="navbar">
+     <ul class="nav_links">
+            <li><a href="home.html" style="color: #000000">HOME</a>  </li>
+            <li><a href="product.html"style="color: #000000">PRODUCTS</a>  </li>
+            <li><a href="about.html" style="color: #FFFFFF">ABOUT</a>  </li>
+            <li><a href="contact.html" style="color: #FFFFFF">CONTACT US</a>    </li>
+          </ul>
+</nav>
+      <div class="search">
+       <button type="Submit">Search</button>
+        <input type="text" class="txt"  placeholder="&emsp;&emsp;Search" >
+</div>
+</header>
+<div class="img" >
+    <p>
+        <br><br>
+    COSTUMIZE
+	<br>
+	YOUR
+	<br>
+	OWN
+	<br>
+	CANDLE
+    <br>
+     <button type="Submit">SHOP NOW</button>
+       <p>
+</div>
+</body>
+</html>
